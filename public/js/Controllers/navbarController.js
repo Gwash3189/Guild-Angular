7b8b2a8d571748c44bc0cc59b966e4a02fa723ec
Adam <adamb3189@gmail.com>
2013-05-31 22:05:09 +1000

    Fixed dynamic nav bar with sub-urls

--- public/js/Controllers/navbarController.js
+++ public/js/Controllers/navbarController.js
@@ -1,9 +1,18 @@
 app.controller('navbarController', function ($scope, $location){
 	$scope.navClass = function (page) {
+		if($location.path().substring(1).split("/").length > 1)// if the location has more than one '/' in it that means that it is sub-url.
+		{
+				if($location.path().substring(1).indexOf(page) != -1){
+
+				//If the sub url has the page varaible in it
+				return 'active'; //return the class active
+			}
+		}
         var currentRoute = $location.path().substring(1) || '';
         return page === currentRoute ? 'active' : '';
     };
 
+    // all of the nav elements in the nav bar.
 	$scope.navs = [
 		{
 			name: 'Home',
